30a2a25349b92b71d27d6ce427fd90d5b166e1a4
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 10:07:56 -0400

    Productos en ASP.Net

---
+++ MVCproduct/MVCproduct/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCproduct.Models
+{
+    public class Product
+    {
+        [key]
+
+        public int productID { get; set; }
+        [Required]
+        [Display(Description ="Product Name")]
+        public string Name{ get; set; }
+        public string Description { get; set; }
+
+
+
+    }
+}
---
+++ MVCproduct/MVCproduct/Models/keyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MVCproduct.Models
+{
+    internal class keyAttribute : Attribute
+    {
+    }
+}
